074eaad399c32c1315d0bfabe0eee4a3f4762c3e
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2017-02-19 05:24:54 +0200

    Moved routers to /routers folder and added root router

--- server/server.js
+++ server/server.js
@@ -6,12 +6,9 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 require('./server-setup').setup(app);
-app.set('db', require('./database'));
 
-app.use('/api', require('./api/api.js'));
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/views/index.html');
-});
+app.set('db', require('./database'));
+app.use(require('./routers/root'));
 
 // todo: normally in express, you'd call app.listen(..), but it doesn't work out of the box
 // with socket.io. Not sure if it matters, but look into it
