ac8f09fe27dbc23c3cd3d5dbbe35e0f39181779c
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2017-02-21 06:07:20 +0200

    Moved https forcing to it's own function

--- server/server-setup.js
+++ server/server-setup.js
@@ -2,19 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const forceHTTPS = (app) => {
+	app.use(function(req, res, next) {
+		if (req.headers['x-forwarded-proto'] !== 'https') {
+			return res.redirect(['https://', req.get('Host'), req.url].join(''));
+		}
+		return next();
+	});
+};
+
 exports.setup = (app) => {
 	app.isProduction = (app.get('env') === 'production');
 	if(app.isProduction) {
-
-		// force https
-		app.use(function(req, res, next) {
-			if (req.headers['x-forwarded-proto'] !== 'https') {
-				return res.redirect(['https://', req.get('Host'), req.url].join(''));
-			}
-			return next();
-		});
+		forceHTTPS(app);
 	}
-
 	
 	app.use(bodyParser.json()); // for parsing application/json
 	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
