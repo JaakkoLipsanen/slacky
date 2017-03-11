5d432618eebeb413fbedcf34fa65816ee84bddf5
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2017-03-11 23:48:58 +0200

    Renamed /routers -> /routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
-const express = require('express');
-
-const app = express();
+const app = require('express')();
 app.server = require('http').createServer(app);
 
 require('./server-setup').setup(app);
 require('./authentication').setup(app);
 require('./server-socket').setup(app);
 
-app.use(require('./routers/root'));
+app.use(require('./routes'));
 
 // must be app.server instead of app because otherwise socket.io doesn't work or smth??
 app.server.listen(app.get('port'), () => {
