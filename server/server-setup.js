9aa9af8443a989ce06a907e4bbab2a457622dc67
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-04-02 18:43:39 +0300

    Removed webpack middleware from server side

--- server/server-setup.js
+++ server/server-setup.js
@@ -42,13 +42,4 @@ exports.setup = (app) => {
 
 	app.use(bodyParser.json()); // for parsing application/json
 	app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-	// public folders
-//	app.use(express.static('public'));
-
-	// TODO: remove
-	app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css'))); // so that '/css/bootstrap.min.js' works
-
-	// sets up hot-reloading etc
-	require('./webpack-setup').setup(app);
 };
