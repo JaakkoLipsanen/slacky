6c3c7ac08260efc831567211e98a3deb6939082a
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-04-14 12:51:56 +0300

    Add clarifying note to client/ghpages deploy script

--- deploy/deploy-client-ghpages.sh
+++ deploy/deploy-client-ghpages.sh
@@ -8,4 +8,7 @@ $(cd client && yarn run build)
 # git clone git@github.com:JaakkoLipsanen/slacky.git --branch gh-pages
 # and then build the build.js there and copy index.html.
 # right now, this script fails if gh-pages isn't cloned into /dist
+echo 'Note: you must clone the gh-pages branch to /client/dist with
+"git clone -b mybranch --single-branch <<REPO_URL>>"'
+
 $(cd client/dist && git add . && git commit -m "New build" && git push origin gh-pages)
