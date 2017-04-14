818b028098c2700fca31de30ad732f5d65d853bf
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-04-14 13:40:47 +0300

    Implement loading screen (#12)

---
+++ client/src/views/loading.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id='loading-page'>
+		<p>Waking up the server...</p>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'login',
+	mounted() {
+		// display the "Waking up the server.." text only after the time represented
+		// by ALLOWED_LATENCY, so that it is actually displayed only after
+		// unexpectedly long waiting
+		const ALLOWED_LATENCY = 300;
+		setTimeout(() => {
+			$("#loading-page p").css("opacity", 1);
+		}, ALLOWED_LATENCY);
+
+	}
+}
+
+</script>
+
+<style lang='scss' scoped>
+	$text-size: 24px;
+	$text-color: rgb(190, 190, 190);
+
+	#loading-page {
+		p {
+			opacity: 0;
+			transition: opacity 0.3s;
+
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+
+			font-size: $text-size;
+			color: $text-color;
+		}
+	}
+</style>
